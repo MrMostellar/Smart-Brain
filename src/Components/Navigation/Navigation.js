66122bbbecab557e4d6f73d0122cce693783b961
Andrew M <mr.mostellar@gmail.com>
2023-03-09 03:32:01 -0700

    Adding a skeleton of components for the finished app

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// const instead of class because this component will not have state.
+const Navigation = () =>{
+    return(
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className="f4 link dim black underline pa3 pointer"> Sign Out</p>
+        </nav>
+    );
+}
+export default Navigation;
