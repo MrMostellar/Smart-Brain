c3ac03707e46820191b58b27cf23c16d63fc6147
Andrew M <mr.mostellar@gmail.com>
2023-03-17 12:50:11 -0600

    Moved the API call to the backend for better security

--- src/App.js
+++ src/App.js
@@ -72,42 +72,13 @@ class App extends React.Component{
     }
 
     imageSubmit = () => {
-        // Your PAT (Personal Access Token) can be found in the portal under Authentification
-        const PAT = '1be12b20d7574e88b1de317e782e7353';
-        // Specify the correct user_id/app_id pairings
-        // Since you're making inferences outside your app's scope
-        const USER_ID = 'mostellar';       
-        const APP_ID = 'SmartBrain';
-        // Change these to whatever model and image URL you want to use
-        const MODEL_ID = 'face-detection';
-        const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
-        const IMAGE_URL = this.state.input;
-            const raw = JSON.stringify({
-                "user_app_id": {
-                    "user_id": USER_ID,
-                    "app_id": APP_ID
-                },
-                "inputs": [
-                    {
-                        "data": {
-                            "image": {
-                                "url": IMAGE_URL
-                            }
-                        }
-                    }
-                ]
-            });
-        
-            const requestOptions = {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Authorization': 'Key ' + PAT
-                },
-                body: raw
-            };
-        
-            fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+                fetch('http://localhost:3000/imageUrl', {
+                    method: 'post',
+                    headers:{'Content-type': 'application/json'},
+                    body: JSON.stringify({
+                        input: this.state.input
+                    })
+                })
                 .then(response => response.json())
                 .then(result => { 
                     const data = Object.values(result.outputs[0].data).length;
